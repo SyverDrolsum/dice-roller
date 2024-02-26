4df5c4709d4b48d6ec762513099e4886edd0d9b1
Syver <syverdklungsoyr@lewisu.edu>
2024-02-25 22:29:43 -0600

    fucked up

--- script.js
+++ script.js
@@ -9,3 +9,12 @@ function rollDice() {
     document.getElementById("dice4").value = RollIndividualDice();
     document.getElementById("dice5").value = RollIndividualDice();
 }
+
+async function rollDice() {
+    const response = await fetch('https://dice-roller-with-nodejs-sdk.azurewebsites.net/', { method: 'GET' });
+    document.getElementById("dice1").value = RollIndividualDice();
+    document.getElementById("dice2").value = RollIndividualDice();
+    document.getElementById("dice3").value = RollIndividualDice();
+    document.getElementById("dice4").value = RollIndividualDice();
+    document.getElementById("dice5").value = RollIndividualDice();
+}
